aff258d6cd84bf4eb85c86ed599629d12edf3d45
Nícolas Wojcichoski <nicolas.dsw@gmail.com>
2020-07-26 10:49:22 -0300

    feat: jpa auditing
    Signed-off-by: Nícolas Wojcichoski <nicolas.dsw@gmail.com>

--- src/main/java/com/example/demo/config/JpaAuditingConfig.java
+++ src/main/java/com/example/demo/config/JpaAuditingConfig.java
@@ -8,13 +8,13 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+@EnableJpaAuditing
 public class JpaAuditingConfig {
 
 	@Bean
-    public AuditorAware<Long> auditorProvider() {
+	public AuditorAware<Long> auditorProvider() {
 		// TODO get principal id
 		// Long id = SecurityContextHolder.getContext().getAuthentication().getId();
-        return () -> Optional.ofNullable(1L);
-    }
+		return () -> Optional.ofNullable(1L);
+	}
 }
